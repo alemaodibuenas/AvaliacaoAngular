e8af03600b328cfb4fbe7dddf00209fa982a20a0
giovanni.pratto@gmail.com <giovanni.pratto@gmail.com>
2021-11-25 00:26:35 -0400

    usuario read foi implementado

---
+++ src/app/components/views/usuario/usuario.model.ts
@@ -0,0 +1,11 @@
+export interface Usuario {
+    id?: String;
+    nome: String;
+    idade: String;
+    cep: String;
+    estado: String;
+    cidade: String;
+    logradouro: String;
+    bairro: String;
+    numero: String;
+}
